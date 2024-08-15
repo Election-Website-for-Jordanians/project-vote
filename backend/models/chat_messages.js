fe2bb82ad04ec7197ef14b081d118c6c7ec1f93b
Basil <basilabushihab22@gmail.com>
2024-08-15 23:03:52 +0300

    changed database structure

--- backend/models/chat_messages.js
+++ backend/models/chat_messages.js
@@ -2,11 +2,7 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class chat_messages extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
    
   }
   chat_messages.init(
